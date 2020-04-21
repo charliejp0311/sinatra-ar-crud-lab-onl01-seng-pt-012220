007e41783c2451ad4e78041e99ecafbf87661ad5
Charles Pelton <charliejp0311@gmail.com>
2020-04-21 19:25:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,6 +46,7 @@ class ApplicationController < Sinatra::Base
 
   delete '/articles/:id' do
     @article = Article.find_by_id(params["id"])
-    Article.remove(@article)
+    @article.destroy
   end
+
 end
